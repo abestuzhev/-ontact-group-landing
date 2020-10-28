86f2f653dc7f01db9c510268eb72a0ad02a176e3
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2020-10-28 07:47:41 +0300

    add js slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,34 @@
+
+
+const resultsSlider = new Swiper('.results .swiper-container', {
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+const failSlider = new Swiper('.fail .swiper-container', {
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 30,
+  effect: 'fade',
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    clickable: true,
+  },
+
+});
+
 $(function(){
 
 });
